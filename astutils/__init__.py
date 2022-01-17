c244c15ffa9d3d17ca59c0d1bc543395646c0baf
Ioannis Filippidis <jfilippidis@gmail.com>
2022-01-18 00:56:42 +0100

    REF: absolute import

--- astutils/__init__.py
+++ astutils/__init__.py
@@ -2,9 +2,11 @@
 # Copyright 2014-2022 by California Institute of Technology
 # All rights reserved. Licensed under 3-clause BSD.
 #
+from __future__ import absolute_import
 from astutils.ast import Terminal, Operator
 from astutils.ply import Lexer, Parser, rewrite_tables
 try:
-    from ._version import version as __version__
+    import astutils._version as _version
+    __version__ = _version.version
 except ImportError:
     __version__ = None
